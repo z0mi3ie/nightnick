f354b4a9492a0a8c3c5e113f8bcffd3b4e9f6feb
Kyle Vickers <kylevickers@gmail.com>
2021-09-10 15:11:40 -0400

    Added swing to high priority link

--- src/components/LinksItem.js
+++ src/components/LinksItem.js
@@ -2,6 +2,14 @@ import React from "react"
 
 class LinksItem extends React.Component {
     render() {
+
+        if (this.props.link.priority === 1) {
+            console.log("WE HAVE PRIORITY ONE for " + this.props.link.link)
+            return (
+                <li><a className="linklistitem-high animate__animated animate__swing" href={this.props.link.link}>{this.props.link.title}</a></li>
+            )
+        }
+
         return (
             <li><a className="linklistitem" href={this.props.link.link}>{this.props.link.title}</a></li>
         )
--- src/components/NightNickContainer.js
+++ src/components/NightNickContainer.js
@@ -8,45 +8,52 @@ class NightNickContainer extends React.Component {
         links: [
             {
                 id: 0,
-                title: "SHAWN'S CHEMO GoFundMe PAGE",
+                title: ">>> SHAWN'S CHEMO GoFundMe PAGE <<<",
                 link: "https://gofund.me/1d0e2b98",
                 image: "",
+                priority: 1,
             },
             {
                 id: 1,
                 title: "visit my shop!",
                 link: "http://www.etsy.com/shop/nightnick",
                 image: "",
+                priority: 0,
             },
             {
                 id: 2,
                 title: "custom pocket pal woodland critter",
                 link: "https://www.etsy.com/listing/1012764435/customize-your-own-pocket-pal-woodland?ref=shop_home_active_8&ltclid=",
                 image: "",
+                priority: 0,
             },
             {
                 id: 3,
                 title: "request for custom pet kitty",
                 link: "https://www.etsy.com/listing/994750181/custom-pet-cat-figurine-customizable?ref=shop_home_feat_1&ltclid=",
                 image: "",
+                priority: 0,
             },
             {
                 id: 4,
                 title: "instagram",
                 link: "https://www.instagram.com/nightnick_?ltclid=",
                 image: "",
+                priority: 0,
             },
             {
                 id: 5,
                 title: "tiktok",
                 link: "https://vm.tiktok.com/ZMJwQaJDB/?ltclid=",
                 image: "",
+                priority: 0,
             },
             {
                 id: 6,
                 title: "amazon wish list",
                 link: "https://www.amazon.com/hz/wishlist/ls/2U9SCZUP9UTYI?ref_=wl_share&ltclid=",
                 image: "",
+                priority: 0,
             },
         ]
     }
